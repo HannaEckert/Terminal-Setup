1f809593570edbc06cb1581f62ef15ca4c47ef55
Hanna Eckert <hanna.eckert@u2d.de>
2024-07-11 09:54:08 +0200

    Adding sudo to the font installation.

--- jetBrainsMono/install.sh
+++ jetBrainsMono/install.sh
@@ -6,7 +6,7 @@ echo "Installing fontconfig"
 sudo apt install fontconfig
 
 echo "Installing JetBrainsMono NFM Regular"
-cp $currentDirectory/JetBrainsMonoNerdFontMono-Regular.ttf /usr/share/fonts/
+sudo cp $currentDirectory/JetBrainsMonoNerdFontMono-Regular.ttf /usr/share/fonts/
 fc-cache -f -v
 
 exit 0
