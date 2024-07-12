d749248b5b5a123ff53761f2246b4c0b898ede18
Hanna Eckert <eckerthanna@icloud.com>
2024-07-13 00:33:34 +0200

    Adding feline instead of lualine. Adding atuin history archive.

---
+++ neovim/nvim/lua/plugins/feline.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"kyazdani42/nvim-web-devicons",
+		lazy = false,
+		config = function()
+			require("nvim-web-devicons").setup()
+		end
+	},
+	{
+		'freddiehaddad/feline.nvim',
+		lazy = false,
+		config = function()
+			require("feline").setup({
+				components = require('catppuccin.groups.integrations.feline').get(),
+			})
+		end
+	}
+}
