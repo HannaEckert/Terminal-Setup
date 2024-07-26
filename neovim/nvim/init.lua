6d2a8f48fcdebd3de91af1c285ac8a533a63dba9
Hanna Eckert <hanna.eckert@u2d.de>
2024-07-26 09:34:39 +0200

    Adding more fine-tuning to the nvim and vscode config.

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,5 +1,4 @@
 -- Basic settings
-vim.cmd("language en_US") -- Set the layout to English
 vim.o.number = true -- Enable line numbers
 vim.o.relativenumber = true -- Enable relative line numbers
 vim.o.tabstop = 4 -- Number of spaces a tab represents
@@ -16,35 +15,35 @@ vim.o.scrolloff = 10 -- Vertical scroll offset
 vim.o.sidescrolloff = 10 -- Horizontal scroll offset
 
 -- Syntax highlighting and filetype plugins
-vim.cmd("syntax enable")
-vim.cmd("filetype plugin indent on")
+vim.cmd('syntax enable')
+vim.cmd('filetype plugin indent on')
 
 -- Space as the leader
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- Disable the arrow keys
-vim.api.nvim_set_keymap({"n", "i", "v"}, "<Up>", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap({"n", "i", "v"}, "<Down>", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap({"n", "i", "v"}, "<Left>", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap({"n", "i", "v"}, "<Right>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set({'n', 'i', 'v'}, '<Up>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set({'n', 'i', 'v'}, '<Down>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set({'n', 'i', 'v'}, '<Left>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set({'n', 'i', 'v'}, '<Right>', '<Nop>', { noremap = true, silent = true })
 
--- Escape input mode with "jj"
-vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+-- Escape input mode with 'jj'
+vim.keymap.set('i', 'jj', '<ESC>', { silent = true })
 
 -- Easy creation of panes
-vim.keymap.set({"n", "v"}, "<leader>v", ":vsplit<CR>", { silent = true })
-vim.keymap.set({"n", "s"}, "<leader>v", ":split<CR>", { silent = true })
+vim.keymap.set({'n', 'v'}, '<leader>v', ':vsplit<CR>', { silent = true })
+vim.keymap.set({'n', 's'}, '<leader>v', ':split<CR>', { silent = true })
 
 -- Easy navigation between panes
-vim.keymap.set({"n", "v"}, "<leader>h", ":wincmd h<CR>", { silent = true })
-vim.keymap.set({"n", "v"}, "<leader>j", ":wincmd j<CR>", { silent = true })
-vim.keymap.set({"n", "v"}, "<leader>k", ":wincmd k<CR>", { silent = true })
-vim.keymap.set({"n", "v"}, "<leader>l", ":wincmd l<CR>", { silent = true })
-vim.keymap.set({"n", "v"}, "<leader><S-h>", ":wincmd <S-h><CR>", { silent = true })
-vim.keymap.set({"n", "v"}, "<leader><S-j>", ":wincmd <S-j><CR>", { silent = true })
-vim.keymap.set({"n", "v"}, "<leader><S-k>", ":wincmd <S-k><CR>", { silent = true })
-vim.keymap.set({"n", "v"}, "<leader><S-l>", ":wincmd <S-l><CR>", { silent = true })
+vim.keymap.set({'n', 'v'}, '<leader>h', ':wincmd h<CR>', { silent = true })
+vim.keymap.set({'n', 'v'}, '<leader>j', ':wincmd j<CR>', { silent = true })
+vim.keymap.set({'n', 'v'}, '<leader>k', ':wincmd k<CR>', { silent = true })
+vim.keymap.set({'n', 'v'}, '<leader>l', ':wincmd l<CR>', { silent = true })
+vim.keymap.set({'n', 'v'}, '<leader><S-h>', ':wincmd <S-h><CR>', { silent = true })
+vim.keymap.set({'n', 'v'}, '<leader><S-j>', ':wincmd <S-j><CR>', { silent = true })
+vim.keymap.set({'n', 'v'}, '<leader><S-k>', ':wincmd <S-k><CR>', { silent = true })
+vim.keymap.set({'n', 'v'}, '<leader><S-l>', ':wincmd <S-l><CR>', { silent = true })
 
 -- Load plugins
-require("config.lazy")
+require('config.lazy')
