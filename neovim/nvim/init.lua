451ca376d59953157542a64641221513a1783fe3
Hanna Eckert <hanna.eckert@u2d.de>
2024-07-26 10:53:18 +0200

    Adding vscode folding fix. Fixing file-tree shortcut

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -8,6 +8,7 @@ vim.o.smartindent = true -- Automatically indent new lines
 vim.o.wrap = false -- Disable line wrapping
 vim.o.cursorline = false -- Highlight the current line
 vim.o.termguicolors = true -- Enable 24-bit RGB colors
+vim.cmd('language en_US') -- Set the layout to English
 vim.o.ignorecase = true -- Ignore case when searching
 vim.o.smartcase = true -- Except when searching with camelCase
 vim.o.updatetime = 250 -- Decrease update time
@@ -23,27 +24,51 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Disable the arrow keys
-vim.keymap.set({'n', 'i', 'v'}, '<Up>', '<Nop>', { noremap = true, silent = true })
-vim.keymap.set({'n', 'i', 'v'}, '<Down>', '<Nop>', { noremap = true, silent = true })
-vim.keymap.set({'n', 'i', 'v'}, '<Left>', '<Nop>', { noremap = true, silent = true })
-vim.keymap.set({'n', 'i', 'v'}, '<Right>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'i', 'v' }, '<Up>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'i', 'v' }, '<Down>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'i', 'v' }, '<Left>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set({ 'n', 'i', 'v' }, '<Right>', '<Nop>', { noremap = true, silent = true })
 
 -- Escape input mode with 'jj'
 vim.keymap.set('i', 'jj', '<ESC>', { silent = true })
 
 -- Easy creation of panes
-vim.keymap.set({'n', 'v'}, '<leader>v', ':vsplit<CR>', { silent = true })
-vim.keymap.set({'n', 's'}, '<leader>v', ':split<CR>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>v', ':vsplit<CR>', { silent = true })
+vim.keymap.set({ 'n', 's' }, '<leader>v', ':split<CR>', { silent = true })
 
 -- Easy navigation between panes
-vim.keymap.set({'n', 'v'}, '<leader>h', ':wincmd h<CR>', { silent = true })
-vim.keymap.set({'n', 'v'}, '<leader>j', ':wincmd j<CR>', { silent = true })
-vim.keymap.set({'n', 'v'}, '<leader>k', ':wincmd k<CR>', { silent = true })
-vim.keymap.set({'n', 'v'}, '<leader>l', ':wincmd l<CR>', { silent = true })
-vim.keymap.set({'n', 'v'}, '<leader><S-h>', ':wincmd <S-h><CR>', { silent = true })
-vim.keymap.set({'n', 'v'}, '<leader><S-j>', ':wincmd <S-j><CR>', { silent = true })
-vim.keymap.set({'n', 'v'}, '<leader><S-k>', ':wincmd <S-k><CR>', { silent = true })
-vim.keymap.set({'n', 'v'}, '<leader><S-l>', ':wincmd <S-l><CR>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>h', ':wincmd h<CR>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>j', ':wincmd j<CR>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>k', ':wincmd k<CR>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>l', ':wincmd l<CR>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader><S-h>', ':wincmd <S-h><CR>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader><S-j>', ':wincmd <S-j><CR>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader><S-k>', ':wincmd <S-k><CR>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<leader><S-l>', ':wincmd <S-l><CR>', { silent = true })
+
+-- Fixing a folding-issue in vscode
+local vscode = require('vscode-neovim')
+local function mapMove(key, direction)
+  vim.keymap.set('n', key, function()
+    local count = vim.v.count
+    local v = 1
+    local style = 'wrappedLine'
+    if count > 0 then
+      v = count
+      style = 'line'
+    end
+    vscode.action('cursorMove', {
+      args = {
+        to = direction,
+        by = style,
+        value = v
+      }
+    })
+  end, options)
+end
+
+mapMove('k', 'up')
+mapMove('j', 'down')
 
 -- Load plugins
 require('config.lazy')
