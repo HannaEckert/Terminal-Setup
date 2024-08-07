cde755589b277e309242dfda649b8f60ca4c78f6
Hanna Eckert <eckerthanna@icloud.com>
2024-08-07 19:49:27 +0200

    Adding nvim alpha dashboard.

---
+++ neovim/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,21 @@
+return {
+	"goolord/alpha-nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local dashboard = require("alpha.themes.dashboard")
+		local theme = require("alpha.themes.theta")
+
+		theme.buttons.val = {
+			{ type = "text",    val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+			{ type = "padding", val = 1 },
+			dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+			dashboard.button("SPC p", "󰈞  Find file"),
+			dashboard.button("SPC /", "󰊄  Live grep"),
+			dashboard.button("c", "  Configuration", "<cmd>cd ~/.config/nvim/<CR><cmd>Neotree<CR>"),
+			dashboard.button("u", "  Update plugins", "<cmd>Lazy sync<CR>"),
+			dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
+		}
+
+		require("alpha").setup(theme.config)
+	end,
+}
