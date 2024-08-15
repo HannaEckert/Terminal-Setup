bf16cc389c6a83ce3376e904eb90c9618ae47df3
Hanna Eckert <eckerthanna@icloud.com>
2024-08-15 15:17:13 +0200

    Adding lazy package update notify to feline.

--- neovim/nvim/lua/plugins/feline.lua
+++ neovim/nvim/lua/plugins/feline.lua
@@ -10,8 +10,25 @@ return {
 		"freddiehaddad/feline.nvim",
 		lazy = false,
 		config = function()
+			local components = require("catppuccin.groups.integrations.feline").get()
+			local lazyStatus = require("lazy.status")
+
+			-- Component for lazy.nvim upgrades
+			table.insert(components.active[2], {
+				enabled = lazyStatus.has_updates,
+				provider = function()
+					local updates = lazyStatus.updates() .. " "
+					return updates
+				end,
+				hl = {
+					fg = "#ea999c",
+					bg = "#232634",
+					style = "bold",
+				},
+			})
+
 			require("feline").setup({
-				components = require("catppuccin.groups.integrations.feline").get(),
+				components = components,
 			})
 		end,
 	},
