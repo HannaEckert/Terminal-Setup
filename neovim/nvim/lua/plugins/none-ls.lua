a4d7d2e00586f715d99f5d76c3ee57da0e98d2d1
Hanna Eckert <eckerthanna@icloud.com>
2024-08-15 18:59:44 +0200

    Adding auto-install to none-ls.

--- neovim/nvim/lua/plugins/none-ls.lua
+++ neovim/nvim/lua/plugins/none-ls.lua
@@ -1,19 +1,34 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					require("none-ls.diagnostics.eslint"),
+				},
+			})
+
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
 	},
-	config = function()
-		local null_ls = require("null-ls")
-
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				require("none-ls.diagnostics.eslint"),
-			},
-		})
-
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = { "stylua", "jq", "eslint_d" },
+			})
+		end,
+	},
 }
