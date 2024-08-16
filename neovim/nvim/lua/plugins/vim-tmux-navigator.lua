6f5aab79c7266eeb73fce427b4f291c962f71eb2
Hanna Eckert <eckerthanna@icloud.com>
2024-08-17 01:26:32 +0200

    Various config refinements.
    Adding java-home to the zsh config and calling export on env-vars.
    Switching the Harpoon trigger from C-s to C-w (conflicted with split).
    Adding C-h C-j C-k C-l as window pane navigation alternative to nvim.

--- neovim/nvim/lua/plugins/vim-tmux-navigator.lua
+++ neovim/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -11,6 +11,10 @@ return {
 		{ "<leader>j", "<cmd>TmuxNavigateDown<cr>" },
 		{ "<leader>k", "<cmd>TmuxNavigateUp<cr>" },
 		{ "<leader>l", "<cmd>TmuxNavigateRight<cr>" },
+		{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+		{ "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+		{ "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+		{ "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
 		{ "<leader>r", "<cmd>silent w<CR><cmd>silent !tmux send-keys -t :.+ Up Enter<CR>" },
 	},
 }
