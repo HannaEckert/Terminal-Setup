6fa015273b648fbda313dc6131a23a00afbb8725
Hanna Eckert <eckerthanna@icloud.com>
2024-08-20 14:00:02 +0200

    Adding a custom boxlang and cfml lsp / treesitter

--- neovim/nvim/lua/plugins/lsp-config.lua
+++ neovim/nvim/lua/plugins/lsp-config.lua
@@ -27,23 +27,68 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			vim.filetype.add({
-				extension = { cfm = "cf", cfc = "cf", cfs = "cf", bxm = "cf", bx = "cf", bxs = "cf" },
+				extension = {
+					cfm = "cfml",
+					cfc = "cfml",
+					cfs = "cfml",
+					bxm = "boxlang",
+					bx = "boxlang",
+					bxs = "boxlang",
+				},
 			})
 
 			local lspconfig = require("lspconfig")
 			local configs = require("lspconfig.configs")
 
-			-- Add my custom cfml lsp
+			-- Add my custom lsps
 			if not configs.cfmlsp then
 				configs.cfmlsp = {
 					default_config = {
-						cmd = { vim.fn.stdpath("config") .. "/cfmlsp/cfmlsp" },
+						cmd = { vim.fn.stdpath("config") .. "/cfmlsp/lsp" },
 						root_dir = lspconfig.util.root_pattern(".git", ".config"),
-						filetypes = { "cf" },
+						filetypes = { "cfml" },
+						on_attach = function()
+							vim.cmd("silent set syntax=cf")
+						end,
+						on_new_config = function(new_config, new_root_dir)
+							local configPath = vim.fn.stdpath("config") .. "/cfmlsp/profile.xml"
+							local targetConfigPath = new_root_dir .. "/.cfmlsp"
+							local profileFile = targetConfigPath .. "/profile.xml"
+
+							new_config.init_options = {
+								profile = {
+									name = "profile",
+									location = ".cfmlsp",
+								},
+							}
+
+							-- Copy default profile.xml if it doesn't exist yet
+							local file = io.open(profileFile, "r")
+							if file ~= nil then
+								io.close(file)
+							else
+								vim.cmd("silent !mkdir -p " .. targetConfigPath)
+								vim.cmd("silent !cp " .. configPath .. " " .. profileFile)
+							end
+						end,
 					},
 				}
 			end
 
+			if not configs.boxlsp then
+				configs.boxlsp = {
+					default_config = {
+						cmd = { vim.fn.stdpath("config") .. "/boxlsp/lsp" },
+						root_dir = lspconfig.util.root_pattern(".git", ".config"),
+						filetypes = { "boxlang" },
+						on_attach = function()
+							vim.cmd("silent set syntax=cf")
+						end,
+					},
+				}
+			end
+
+			lspconfig.boxlsp.setup({ capabilities = capabilities })
 			lspconfig.cfmlsp.setup({ capabilities = capabilities })
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
 			lspconfig.html.setup({ capabilities = capabilities })
--- neovim/nvim/lua/plugins/treesitter.lua
+++ neovim/nvim/lua/plugins/treesitter.lua
@@ -1,39 +1,59 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			local parserConfig = require("nvim-treesitter.parsers").get_parser_configs()
 
-		configs.setup({
-			ensure_installed = {
-				"lua",
-				"c",
-				"vim",
-				"vimdoc",
-				"query",
-				"javascript",
-				"html",
-				"java",
-				"python",
-				"bash",
-				"cmake",
-				"css",
-				"dockerfile",
-				"gitcommit",
-				"markdown",
-				"perl",
-				"php",
-				"python",
-				"ruby",
-				"sql",
-				"toml",
-				"typescript",
-				"xml",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+			-- Add CFML parser
+			parserConfig.cfml = {
+				install_info = {
+					url = "https://github.com/cfmleditor/tree-sitter-cfml",
+					files = { "src/parser.c", "src/scanner.c" },
+					location = "cfml",
+				},
+				filetype = "cfml", -- Filetype in Neovim to associate with the parser
+			}
+
+			configs.setup({
+				modules = {},
+				ignore_install = {},
+				ensure_installed = {
+					"lua",
+					"c",
+					"vim",
+					"vimdoc",
+					"query",
+					"javascript",
+					"html",
+					"java",
+					"python",
+					"bash",
+					"cmake",
+					"css",
+					"dockerfile",
+					"gitcommit",
+					"markdown",
+					"perl",
+					"php",
+					"python",
+					"ruby",
+					"sql",
+					"toml",
+					"typescript",
+					"xml",
+					"cfml",
+				},
+				sync_install = false,
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/playground",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+	},
 }
