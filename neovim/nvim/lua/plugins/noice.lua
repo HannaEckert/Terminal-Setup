082aca90a624084703de0e64d9084cc5aea5b0ba
Hanna Eckert <eckerthanna@icloud.com>
2024-09-04 23:49:37 +0200

    Adding the noice plugin to nvim.

---
+++ neovim/nvim/lua/plugins/noice.lua
@@ -0,0 +1,23 @@
+return {
+	"folke/noice.nvim",
+	dependencies = { "MunifTanjim/nui.nvim" },
+	config = function()
+		require("noice").setup({
+			cmdline = { view = "cmdline" },
+			messages = { view = "mini" },
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = true,
+				inc_rename = false,
+				lsp_doc_border = false,
+			},
+		})
+	end,
+}
