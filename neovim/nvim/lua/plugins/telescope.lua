ea324cf3eabf7e49019d3cfa128813150a993844
Hanna Eckert <hanna.eckert@u2d.de>
2024-09-25 10:34:14 +0200

    Improving the telescope configuration.
    Adding a mapping to grep search for the highlighted word.
    Adding a mapping to open the current selection as split.

--- neovim/nvim/lua/plugins/telescope.lua
+++ neovim/nvim/lua/plugins/telescope.lua
@@ -5,22 +5,33 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fF", function()
+			vim.keymap.set({ "n", "v" }, "<leader>ff", builtin.find_files, {})
+			vim.keymap.set({ "n", "v" }, "<leader>fF", function()
 				builtin.find_files({ hidden = true })
 			end)
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+			vim.keymap.set("v", "<leader>fg", "y<ESC>:Telescope live_grep default_text=<c-r>0<CR>")
+
+			vim.keymap.set({ "n", "v" }, "<leader>fb", builtin.buffers)
+			vim.keymap.set({ "n", "v" }, "<leader>fh", builtin.help_tags)
 		end,
 	},
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
+			local actions = require("telescope.actions")
+
 			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
+					},
+				},
+				defaults = {
+					mappings = {
+						i = {
+							["<C-a>"] = actions.select_vertical,
+						},
 					},
 				},
 			})
