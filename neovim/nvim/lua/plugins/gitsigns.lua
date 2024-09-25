00136ab0ccf97a508509bed2c20b87b573b789a5
Hanna Eckert <hanna.eckert@u2d.de>
2024-09-25 10:32:40 +0200

    Improving git management in nvim.

--- neovim/nvim/lua/plugins/gitsigns.lua
+++ neovim/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,28 @@
 return {
-	'lewis6991/gitsigns.nvim',
+	"lewis6991/gitsigns.nvim",
 	config = function()
-		require('gitsigns').setup()
-	end
+		local gitsigns = require("gitsigns")
+		gitsigns.setup()
+
+		-- Navigation
+		vim.keymap.set("n", "]c", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "]c", bang = true })
+			else
+				gitsigns.nav_hunk("next")
+			end
+		end)
+
+		vim.keymap.set("n", "[c", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "[c", bang = true })
+			else
+				gitsigns.nav_hunk("prev")
+			end
+		end)
+
+		vim.keymap.set("n", "<leader>gl", gitsigns.blame_line)
+		vim.keymap.set("n", "<leader>gb", gitsigns.blame)
+		vim.keymap.set("n", "<leader>gd", gitsigns.diffthis)
+	end,
 }
