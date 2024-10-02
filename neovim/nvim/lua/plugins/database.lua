9f5642950b3991fcbfa89d9b5107cd736eaa6146
Hanna Eckert <hanna.eckert@u2d.de>
2024-10-02 10:01:38 +0200

    Making the database tui hide the alpha dashboard.

--- neovim/nvim/lua/plugins/database.lua
+++ neovim/nvim/lua/plugins/database.lua
@@ -22,6 +22,6 @@ return {
 		vim.g.db_ui_show_database_icon = 1
 		vim.g.db_ui_use_nvim_notify = 1
 
-		vim.keymap.set({ "n", "v" }, "<leader>sd", ":DBUIToggle<cr>", { silent = true })
+		vim.keymap.set({ "n", "v" }, "<leader>sd", ":enew<cr>:DBUIToggle<cr>", { silent = true })
 	end,
 }
