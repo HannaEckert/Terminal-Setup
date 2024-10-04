4141a12e56f662f5261744e6627895ff5c59b108
Hanna Eckert <hanna.eckert@u2d.de>
2024-10-04 11:08:02 +0200

    Adding comment strings for boxlang and cfml code.

--- neovim/nvim/lua/plugins/comment.lua
+++ neovim/nvim/lua/plugins/comment.lua
@@ -1,1 +1,21 @@
-return { "numToStr/Comment.nvim" }
+return {
+	"numToStr/Comment.nvim",
+	config = function()
+		-- Setup the correct comment string
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+			pattern = { "*.cfc", "*.cfs", "*.bx", "*.bxs" },
+			callback = function()
+				vim.opt.commentstring = "// %s"
+			end,
+		})
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+			pattern = { "*.cfm", "*.bxm" },
+			callback = function()
+				vim.opt.commentstring = "<!--- %s --->"
+			end,
+		})
+
+		require("Comment").setup()
+	end,
+}
