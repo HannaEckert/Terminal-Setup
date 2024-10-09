8d4c98abe9721b00e828c6710a5be2cd0603f29c
Hanna Eckert <hanna.eckert@u2d.de>
2024-10-09 07:55:56 +0200

    Adding a harpoon shortcut for file #6.

--- neovim/nvim/lua/plugins/harpoon.lua
+++ neovim/nvim/lua/plugins/harpoon.lua
@@ -14,5 +14,6 @@ return {
 		vim.keymap.set({ "n", "v" }, "<leader>3", function() harpoon:list():select(3) end)
 		vim.keymap.set({ "n", "v" }, "<leader>4", function() harpoon:list():select(4) end)
 		vim.keymap.set({ "n", "v" }, "<leader>5", function() harpoon:list():select(5) end)
+		vim.keymap.set({ "n", "v" }, "<leader>6", function() harpoon:list():select(6) end)
 	end
 }
