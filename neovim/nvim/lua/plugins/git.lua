c7015bb3a91cf021ebdd975dd818142339896844
Hanna Eckert <hanna.eckert@u2d.de>
2024-11-05 15:43:55 +0100

    Adding the LazyGit neovim plugin.

---
+++ neovim/nvim/lua/plugins/git.lua
@@ -0,0 +1,47 @@
+return {
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			local gitsigns = require("gitsigns")
+			gitsigns.setup()
+
+			-- Navigation
+			vim.keymap.set("n", "]c", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "]c", bang = true })
+				else
+					gitsigns.nav_hunk("next")
+				end
+			end)
+
+			vim.keymap.set("n", "[c", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "[c", bang = true })
+				else
+					gitsigns.nav_hunk("prev")
+				end
+			end)
+
+			vim.keymap.set("n", "<leader>gl", gitsigns.blame_line)
+			vim.keymap.set("n", "<leader>gb", gitsigns.blame)
+			vim.keymap.set("n", "<leader>gd", gitsigns.diffthis)
+		end,
+	},
+}
