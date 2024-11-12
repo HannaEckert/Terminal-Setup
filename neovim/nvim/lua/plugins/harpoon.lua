66b71a707d81fb445ce20f638814f536a7c23b21
Hanna Eckert <hanna.eckert@u2d.de>
2024-11-12 10:19:18 +0100

    Making harpoon git branch sensible.

--- neovim/nvim/lua/plugins/harpoon.lua
+++ neovim/nvim/lua/plugins/harpoon.lua
@@ -3,8 +3,31 @@ return {
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
+		-- Function to get the current Git branch
+		local function get_git_branch()
+			local handle = io.popen("git rev-parse --abbrev-ref HEAD 2>/dev/null")
+			local defaultKey = "no-git"
+
+			print("GOT CALLED")
+			if handle then
+				local branch = handle:read("*a"):gsub("%s+", "")
+				branch = string.gsub(branch, "/", "_")
+				handle:close()
+
+				if branch ~= "" then
+					return branch
+				end
+			end
+
+			return defaultKey
+		end
+
 		local harpoon = require("harpoon")
-		harpoon:setup()
+		harpoon:setup({
+			settings = {
+				key = get_git_branch
+			}
+		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>a", function() harpoon:list():add() end)
 		vim.keymap.set({ "n", "v" }, "<leader>fa", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
