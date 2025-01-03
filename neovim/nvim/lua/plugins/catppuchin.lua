5a11749f980a381db9d4163325d3b8ed96fece5d
Hanna Eckert <hanna.eckert@u2d.de>
2025-01-03 11:12:28 +0100

    Optimizing the neovim theming.

--- neovim/nvim/lua/plugins/catppuchin.lua
+++ neovim/nvim/lua/plugins/catppuchin.lua
@@ -4,6 +4,17 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+		require("catppuccin").setup({
+			integrations = {
+				harpoon = true,
+				headlines = true,
+				noice = true,
+				notify = true,
+				nvim_surround = true,
+				dadbod_ui = true,
+			}
+		})
+
 		vim.cmd.colorscheme("catppuccin-frappe")
 	end,
 }
--- neovim/nvim/lua/plugins/markdown.lua
+++ neovim/nvim/lua/plugins/markdown.lua
@@ -1,11 +1,21 @@
 return {
 	{
-		'MeanderingProgrammer/render-markdown.nvim',
-		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	},
 	{
-		'opdavies/toggle-checkbox.nvim',
+		"opdavies/toggle-checkbox.nvim",
 		config = function()
+			require("render-markdown").setup({
+				heading = {
+					icons = { " ", " ", " ", " ", "󱁇 " ,"󱁄 "},
+					position = "inline",
+					width = "block",
+					min_width = 80,
+					left_pad = 2,
+				},
+			})
+
 			vim.keymap.set("n", "<leader>t", ":lua require('toggle-checkbox').toggle()<CR>", { silent = true })
 
 			vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
@@ -14,7 +24,6 @@ return {
 					vim.o.signcolumn = "yes"
 				end,
 			})
-
-		end
-	}
+		end,
+	},
 }
