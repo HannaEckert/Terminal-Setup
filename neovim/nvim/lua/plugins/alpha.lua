7d30ad237e5ef6b31d4097ed25115d4da0f586a7
Hanna Eckert <hanna.eckert@u2d.de>
2025-01-08 12:29:44 +0100

    Deciding on a multicolored neovim alpha header.

--- neovim/nvim/lua/plugins/alpha.lua
+++ neovim/nvim/lua/plugins/alpha.lua
@@ -5,7 +5,53 @@ return {
 		local alpha = require("alpha")
 		local startify = require("alpha.themes.startify")
 
-		startify.section.header.val = ""
+		local function getCharLen(s, pos)
+			local byte = string.byte(s, pos)
+			if not byte then
+				return nil
+			end
+			return (byte < 0x80 and 1) or (byte < 0xE0 and 2) or (byte < 0xF0 and 3) or (byte < 0xF8 and 4) or 1
+		end
+
+		local function applyColors(logo, colors, logoColors)
+			startify.section.header.val = logo
+
+			for key, color in pairs(colors) do
+				local name = "Alpha" .. key
+				vim.api.nvim_set_hl(0, name, color)
+				colors[key] = name
+			end
+
+			startify.section.header.opts.hl = {}
+			for i, line in ipairs(logoColors) do
+				local highlights = {}
+				local pos = 0
+
+				for j = 1, #line do
+					local opos = pos
+					pos = pos + getCharLen(logo[i], opos + 1)
+
+					local color_name = colors[line:sub(j, j)]
+					if color_name then
+						table.insert(highlights, { color_name, opos, pos })
+					end
+				end
+
+				table.insert(startify.section.header.opts.hl, highlights)
+			end
+		end
+
+		applyColors({
+			[[   ]],
+			[[    n   e       v   i   m    ]],
+		}, {
+			["a"] = { fg = "#51576d" }, -- gray / surface 1
+			["b"] = { fg = "#e5c890" }, -- yellow
+		}, {
+			[[ a ]],
+			[[    a   a   b    a   a   a    ]],
+		})
+
 		startify.section.top_buttons.val = {}
 		startify.section.mru.val = {}
 
