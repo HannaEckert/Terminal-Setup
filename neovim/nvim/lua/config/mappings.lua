22c549cbcb34126f769673e07bf683067f759f47
Hanna Eckert <hanna.eckert@u2d.de>
2025-01-10 08:20:47 +0100

    Removing <leader> window navigation within neovim.

--- neovim/nvim/lua/config/mappings.lua
+++ neovim/nvim/lua/config/mappings.lua
@@ -2,12 +2,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Make the arrow keys resize splits
-vim.keymap.set({ "n", "i", "v" }, "<Up>", "<esc>:silent! resize +2<CR>", { silent = true })
-vim.keymap.set({ "n", "i", "v" }, "<Down>", "<esc>:silent! resize -2<CR>", { silent = true })
-vim.keymap.set({ "n", "i", "v" }, "<Left>", "<esc>:silent! vertical resize -2<CR>", { silent = true })
-vim.keymap.set({ "n", "i", "v" }, "<Right>", "<esc>:silent! vertical resize +2<CR>", { silent = true })
-
 -- Escape input mode with 'jj'
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
 
@@ -21,16 +15,6 @@ vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { silent = true })
 -- Delete and paste without yank
 vim.keymap.set({ "n", "v" }, "<leader>p", '"_p', { silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>d", '"_d', { silent = true })
-
--- Easy navigation between panes
-vim.keymap.set({ "n", "v" }, "<leader>h", ":wincmd h<CR>", { silent = true })
-vim.keymap.set({ "n", "v" }, "<leader>j", ":wincmd j<CR>", { silent = true })
-vim.keymap.set({ "n", "v" }, "<leader>k", ":wincmd k<CR>", { silent = true })
-vim.keymap.set({ "n", "v" }, "<leader>l", ":wincmd l<CR>", { silent = true })
-vim.keymap.set({ "n", "v" }, "<leader><S-h>", ":wincmd <S-h><CR>", { silent = true })
-vim.keymap.set({ "n", "v" }, "<leader><S-j>", ":wincmd <S-j><CR>", { silent = true })
-vim.keymap.set({ "n", "v" }, "<leader><S-k>", ":wincmd <S-k><CR>", { silent = true })
-vim.keymap.set({ "n", "v" }, "<leader><S-l>", ":wincmd <S-l><CR>", { silent = true })
 
 -- Easy close file or editor
 vim.keymap.set({ "n", "v" }, "<leader>q", "<cmd>:q<CR>", { silent = true })
