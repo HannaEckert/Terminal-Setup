5b6e6049ccd23310e87f4ecd2af66b2bc6ff5899
Hanna Eckert <eckerthanna@icloud.com>
2025-03-25 15:34:37 +0000

    Adjusting the tmux config.

--- zsh/zsh/tmux_connect.zsh
+++ zsh/zsh/tmux_connect.zsh
@@ -1,5 +1,5 @@
 # If not iside tmux, reconnect to the last tmux session or open a new one
 if [[ -z $SKIP_TMUX_INIT && -z $TMUX ]] ; then
 	clear
-	tmux -u a 2>/dev/null || tmux -u
+	tmux a 2>/dev/null || tmux
 fi
